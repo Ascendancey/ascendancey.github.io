d4cf7de5e46d9ee40067b713e122b0c3bde2354b
Mikhail Petukhov <mikhail.petukhov@yahoo.com>
2022-06-13 18:15:08 +0200

    New view + CV section

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import CV from './Components/CV';
+// import Contact from './Components/Contact';
+// import Testimonials from './Components/Testimonials';
+// import Portfolio from './Components/Portfolio';
+import * as data from './cvData.json';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foo: 'bar',
+      cvData: data,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header data={this.state.cvData.main} />
+        <About data={this.state.cvData.main} />
+        <CV data={this.state.cvData.CV} />
+        {/* <Portfolio data={this.state.cvData.portfolio} /> */}
+        {/* <Testimonials data={this.state.cvData.testimonials} /> */}
+        {/* <Contact data={this.state.cvData.main} /> */}
+        <Footer data={this.state.cvData.main} />
+      </div>
+    );
+  }
+}
+
+export default App;
