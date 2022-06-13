f2497fa30c06413dd174dda5cfa88cdaad8784f0
Mikhail Petukhov <mikhail.petukhov@yahoo.com>
2022-06-13 19:17:45 +0200

    Added websites to companies in CV

--- src/Components/CV.js
+++ src/Components/CV.js
@@ -19,7 +19,9 @@ class CV extends Component {
       var work = this.props.data.work.map(function(work) {
         return (
           <div key={work.company}>
-            <h3>{work.company}</h3>
+            <a href={work.link}>
+              <h3>{work.company}</h3>
+            </a>
             <p className="info">
               {work.title}
               <span>&bull;</span> <em className="date">{work.years}</em>
